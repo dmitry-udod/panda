47866c585913ccc5663c4b24c943fa2bbca2ef54
Dmitry Udod <udod.dmitry@gmail.com>
2016-09-20 17:35:54 +0300

    Language select

---
+++ resources/lang/en/shop.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'table.name' => 'Name',
+    'table.address' => 'Адреса',
+    'table.what_do' => 'Вид діяльності',
+    'table.owner' => 'Власник',
+    'table.open' => 'Відчиняєтся',
+    'table.close' => 'Зачиняєтся',
+    'work_24_hours' => 'Цілодобово',
+];
